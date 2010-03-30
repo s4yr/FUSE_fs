e29a13bff31645dabd406947c30e6766d4963f06
s4yr <s4yrmc@gmail.com>
2010-03-31 01:44:13 +0300

    fuse_almost_done

--- main2.c
+++ main2.c
@@ -1,57 +1,34 @@
-
 #define FUSE_USE_VERSION 26
-#define _XOPEN_SOURCE 500
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <fuse.h>
 #include <stdio.h>
 #include <string.h>
-#include <errno.h>
-#include <fuse.h>
+#include <unistd.h>
 #include <fcntl.h>
-#include <stddef.h>
-#include <fuse_opt.h>
 #include <dirent.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <sys/time.h>
+#include "functions.h"
+#ifdef HAVE_SETXATTR
+#include <sys/xattr.h>
+#endif
 char name1[10] = "/file";	
 char name2[10];
 static int savefd;
 char* intToChar(char* bufer, int numb);
 static struct fuse_session* se;
-struct mbr_data
-{
-  int fd;
-  uint8_t ro;
-  char *filename;
- // struct mbr_partition *primary;
-};
+
 static int pr_getattr(const char* path,struct stat* st_buf)		//get attributes about file/directory
 {
-	int res = 0;	//temporary result
-	char* tmp_path;
-	int lenght = strlen(path) - 1;
-	memset(st_buf,0,sizeof(struct stat));
-	tmp_path = (char*)calloc(strlen(path)+1,sizeof(char));
-	//path = fixpath(path);
-	res = stat(path,st_buf);
-	fprintf(2,"saasd12");
-	return res;
+	int res;
 
+	res = lstat(path, st_buf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
 
 }
+
 static int pr_fgetattr(const char* path, struct stat* st_buf)
 {
 	int res;
@@ -67,63 +44,101 @@ static int pr_access(const char *path, int mask)
 	res = access(path, mask);
 	if (res == -1)
 		return -errno;
-
 	return 0;
 }
-static int xmp_readlink(const char *path, char *buf, size_t size)
+static int pr_readlink(const char *path, char *buf, size_t size)		//read symbolik link -??????
 {
 	int res;
 
 	res = readlink(path, buf, size - 1);
 	if (res == -1)
 		return -errno;
-
 	buf[res] = '\0';
 	return 0;
 }
-static inline DIR *get_dirp(struct fuse_file_info *fi)		//return directory pointer
-{
-    return (DIR *) (uintptr_t) fi->fh;
-}
-int pr_opendir(const char *path, struct fuse_file_info *fi)
-{
-	DIR* dp;
-	//path = fixpath(path);
-	dp = opendir(path);
-	if(dp = NULL)
-	{
-		printf("errr");
-		return -errno;
-	}
-	fi->fh = (unsigned long) dp;
-	return 0;		
-}
+//read content of current directory
 static int pr_readdir(const char* path, void* buf, fuse_fill_dir_t filler,off_t offset,struct fuse_file_info* fi)
 {
-	int res = 0;
-	struct stat st;
-	struct dirent* de;
-	DIR* dp = get_dirp(fi);
-	(void)path;
-	seekdir(dp,offset);
-	while((de = readdir(dp)) != NULL)
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(path);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) 
 	{
 		struct stat st;
-		memset(&st,0,sizeof(st));
+		memset(&st, 0, sizeof(st));
 		st.st_ino = de->d_ino;
-		st.st_mode = de->d_type << 12;
-		if(filler(buf,de->d_name,&st,telldir(dp)))
-				break;
+		st.st_mode = de->d_type << 12;	
+		if (filler(buf, de->d_name, &st, 0))
+			break;
 	}
+	closedir(dp);
+	return 0;
+}
+// fuse realization of mkdir
+static int pr_mkdir(const char *path, mode_t mode)
+{
+	int res;
+	res = mkdir(path, mode);
+	if (res == -1)
+		return -errno;
+	return 0;
+}
+//fuse realization of delete dir
+static int pr_rmdir(const char *path)
+{
+	int res;
+
+	res = rmdir(path);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+//read file content
+static int pr_read(const char *path, char *buf, size_t size, off_t offset,
+		    struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+
+	(void) fi;
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return -errno;
+	res = pread(fd, buf, size, offset);	//read file contenxt
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
 	return res;
 }
+//open file
+static int pr_open(const char *path, struct fuse_file_info *fi)
+{
+	int res;
+
+	res = open(path, fi->flags);
+	if (res == -1)
+		return -errno;
+	close(res);
+	return 0;
+}
 static struct fuse_operations pr_operations = {
-	.getattr 	= pr_getattr,
-	.readlink	= xmp_readlink,
-	//.fgetattr	= pr_fgetattr,
-	//.readdir	= pr_readdir, 
-	//.open		= pr_opendir,
-	//.access		= pr_access,
+	.getattr		= pr_getattr,
+	.access			= pr_access,
+	.readlink		= pr_readlink,
+	.readdir		= pr_readdir,
+	.mkdir			= pr_mkdir,
+	.rmdir			= pr_rmdir,
+	.read			= pr_read,
 	};	
 int main(int argc,char* argv[])
 {
@@ -131,38 +146,6 @@ int main(int argc,char* argv[])
 	int ret;	
 	umask(0);
 	struct fuse_args args;
-	//args = (const struct fuse_args) FUSE_ARGS_INIT (argc,argv);
-	//se = fuse_lowlevel_new (&args,&pr_operations,sizeof(pr_operations),NULL);
-	ret = fuse_main(argc,argv,&pr_operations,NULL);
-	printf("%d",ret);
+	ret = fuse_main(argc,argv,&pr_operations,NULL);;
 	return ret;
-}
-char* intToChar(char* bufer, int numb)
-{
-	char tmp[20];
-	int buf;
-	int lenght = 0;
-	int i = 0;
-	int j;
-	if(numb >= 10)
-	{
-		do
-		{
-			buf = numb%10;
-			tmp[i] = buf + '0';
-			i++;
-			numb /=10;		
-		} while (numb >= 10);
-		tmp[i] = numb + '0';
-	}else
-	{
-		tmp[i] = numb + '0';
-	};
-	lenght = i;
-	for (j = 0; j <= lenght; j++)
-	{
-		bufer[j] = tmp[i--];
-	}
-	bufer[j] = NULL;
-	return bufer;	
 }
