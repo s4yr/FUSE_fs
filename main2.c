66f1f88af5e2128cdf77471d010b7f38865d1407
s4yr <s4yrmc@gmail.com>
2010-04-06 01:14:17 +0300

    add_to fuse_module

--- main2.c
+++ main2.c
@@ -7,6 +7,9 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <gcrypt.h>
 #include "functions.h"
 #ifdef HAVE_SETXATTR
 #include <sys/xattr.h>
@@ -24,11 +27,8 @@ static int pr_getattr(const char* path,struct stat* st_buf)		//get attributes ab
 	res = lstat(path, st_buf);
 	if (res == -1)
 		return -errno;
-
 	return 0;
-
 }
-
 static int pr_fgetattr(const char* path, struct stat* st_buf)
 {
 	int res;
@@ -81,13 +81,28 @@ static int pr_readdir(const char* path, void* buf, fuse_fill_dir_t filler,off_t 
 	closedir(dp);
 	return 0;
 }
+//syslog
+//var/log/messages
 // fuse realization of mkdir
+static int my_printf()
+{
+	size_t len = 20;
+	char *tmp = alloca (len + 2);
+	printf("qwerty\n");
+	write(1,"qwe\n",5);
+	write(2,"qwert\n",7);
+	return 0;
+}
 static int pr_mkdir(const char *path, mode_t mode)
 {
 	int res;
 	res = mkdir(path, mode);
 	if (res == -1)
 		return -errno;
+	FILE* fp = fopen( "/tmp/afuse.log", "w+" );
+	fprintf( fp, "PRINT FROM FUSE: %s\n",path);
+	fclose(fp);
+	my_printf();
 	return 0;
 }
 //fuse realization of delete dir
@@ -108,15 +123,14 @@ static int pr_read(const char *path, char *buf, size_t size, off_t offset,
 {
 	int fd;
 	int res;
-
 	(void) fi;
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
 		return -errno;
+	offset= 3;
 	res = pread(fd, buf, size, offset);	//read file contenxt
 	if (res == -1)
 		res = -errno;
-
 	close(fd);
 	return res;
 }
@@ -131,6 +145,59 @@ static int pr_open(const char *path, struct fuse_file_info *fi)
 	close(res);
 	return 0;
 }
+//-----------------------------------------------------------
+static int pr_write(const char *path, const char *buf, size_t size,
+		     off_t offset, struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+
+	(void) fi;
+	fd = open(path, O_WRONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pwrite(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static int pr_statfs(const char *path, struct statvfs *stbuf)
+{
+	int res;
+
+	res = statvfs(path, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int pr_release(const char *path, struct fuse_file_info *fi)
+{
+	/* Just a stub.	 This method is optional and can safely be left
+	   unimplemented */
+
+	(void) path;
+	(void) fi;
+	return 0;
+}
+
+static int pr_fsync(const char *path, int isdatasync,
+		     struct fuse_file_info *fi)
+{
+	/* Just a stub.	 This method is optional and can safely be left
+	   unimplemented */
+
+	(void) path;
+	(void) isdatasync;
+	(void) fi;
+	return 0;
+}
+//
 static struct fuse_operations pr_operations = {
 	.getattr		= pr_getattr,
 	.access			= pr_access,
